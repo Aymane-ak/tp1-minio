57e93e9dda058d31e3cee3ceb962b25fa5fc46c1
aymane-ak <aymaneak30@gmail.com>
2025-11-29 23:06:43 +0100

    feature : fixing main and adding variables

--- main.tf
+++ main.tf
@@ -1,21 +1,33 @@
+terraform {
+  required_providers {
+    minio = {
+      source  = "terraform-provider-minio/minio"
+      version = "3.1.0"
+    }
+  }
+}
+
 provider "minio" {
- minio_server = "127.0.0.1:9000"
- minio_user = "minioadmin"
- minio_password = "minioadmin"
+  minio_server   = var.minio_server
+  minio_user     = var.minio_user      
+  minio_password = var.minio_password   
+  minio_ssl      = false
 }
+
+
 resource "minio_s3_bucket" "web_bucket" {
- bucket = "webbucket"
- acl = "public-read"
+  bucket = var.web_bucket_name
+  acl    = "public-read"
 }
+
 resource "minio_s3_object" "index_html" {
- bucket = minio_s3_bucket.web_bucket.bucket
- object = "index.html"
- source = "index.html"
- acl = "public-read"
+  bucket_name = minio_s3_bucket.web_bucket.bucket
+  object_name = "index.html"
+  source      = "index.html"
 }
+
 resource "minio_s3_object" "style_css" {
- bucket = minio_s3_bucket.web_bucket.bucket
- object = "style.css"
- source = "style.css"
- acl = "public-read"
+  bucket_name = minio_s3_bucket.web_bucket.bucket
+  object_name = "style.css"
+  source      = "style.css"
 }
